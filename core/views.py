3ba2e5dbeb0c6e0bafa36d5e0b30b90afca4f1e8
oleg <olegsh-gusev@mail.ru>
2021-06-10 19:44:14 +0300

    reverted

--- core/views.py
+++ core/views.py
@@ -9,6 +9,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from core import models
+from fb_parser.bot.bot import login
 from fb_parser.tasks import start_parsing_by_keyword, start_first_update_posts, add_work_credential, add_proxy
 from fb_parser.utils.find_data import find_value
 
@@ -17,6 +18,26 @@ from fb_parser.utils.find_data import find_value
 @api_view(["GET"])
 @permission_classes((AllowAny,))
 def test(request):
+    proxy = {'http': 'http://test:test@131.153.151.250:20805', 'https': 'https://test:test@131.153.151.250:20805'}
+
+    session = requests.session()
+    session.proxies.update(proxy)
+    session.headers.update({
+        'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:39.0) Gecko/20100101 Firefox/39.0'
+    })
+    print('session start')
+
+    fb_dtsg, user_id, xs, token = login(session, "79617928238", 'yAUqcXLk25221')
+
+    q = 'key'
+    url = 'https://m.facebook.com/search/posts/?q=%s&source=filter&pn=8&isTrending=0&' \
+          'fb_dtsg_ag=%s&__a=AYlcAmMg3mcscBWQCbVswKQbSUum-R7WYoZMoRSwBlJp6gjuv2v2LwCzB_1ZZe4khj4N2vM7UjQWttgYqsq7DxeUlgmEVmSge5LOz1ZdWHEGQQ' % (
+              q, token)
+
+    headers = {'cookie': 'c_user=' + user_id + '; xs=' + xs + ';'}
+
+    res = requests.get(url, headers=headers, proxies=proxy)
+
     return Response({'token': models.PostContent.objects.all().first().post_id})
 
     return models.Post.objects.all()
--- fb_parser/celery/celery.py
+++ fb_parser/celery/celery.py
@@ -15,20 +15,20 @@ app.config_from_object('django.conf:settings')
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
-    # 'start_first_update_posts': {
-    #     'task': 'fb_parser.tasks.start_first_update_posts',
-    #     'schedule': crontab(minute='*/1')
-    # },
+    'start_first_update_posts': {
+        'task': 'fb_parser.tasks.start_first_update_posts',
+        'schedule': crontab(minute='*/1')
+    },
     'start_parsing_by_keyword': {
         'task': 'fb_parser.tasks.start_parsing_by_keyword',
         'schedule': crontab(minute='*/1')
     },
-    # 'add_work_credential': {
-    #     'task': 'fb_parser.tasks.add_work_credential',
-    #     'schedule': crontab(minute='*/1')
-    # },
-    # 'add_proxy': {
-    #     'task': 'fb_parser.tasks.add_proxy',
-    #     'schedule': crontab(minute='*/1')
-    # },
+    'add_work_credential': {
+        'task': 'fb_parser.tasks.add_work_credential',
+        'schedule': crontab(minute='*/1')
+    },
+    'add_proxy': {
+        'task': 'fb_parser.tasks.add_proxy',
+        'schedule': crontab(minute='*/1')
+    },
 }
