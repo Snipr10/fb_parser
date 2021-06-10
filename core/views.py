70545c7e44708be35a2d20c414c1a27119b9ae31
oleg <olegsh-gusev@mail.ru>
2021-06-10 12:35:30 +0300

    test

--- core/views.py
+++ core/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 from core import models
 from fb_parser.tasks import start_parsing_by_keyword, start_first_update_posts, add_work_credential, add_proxy
@@ -16,5 +17,7 @@ from fb_parser.utils.find_data import find_value
 @api_view(["GET"])
 @permission_classes((AllowAny,))
 def test(request):
+    return Response({'token': 'OK'})
+
     return models.Post.objects.all()
     start_first_update_posts()
