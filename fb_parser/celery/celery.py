472fb96fb8ad9e024d4c153b7c254e0c19beb97d
oleg <olegsh-gusev@mail.ru>
2021-06-30 09:50:15 +0300

    test

--- fb_parser/celery/celery.py
+++ fb_parser/celery/celery.py
@@ -17,11 +17,11 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'start_first_update_posts': {
         'task': 'fb_parser.tasks.start_first_update_posts',
-        'schedule': crontab(minute='*/10')
+        'schedule': crontab(minute='*/5')
     },
     'start_parsing_by_keyword': {
         'task': 'fb_parser.tasks.start_parsing_by_keyword',
-        'schedule': crontab(minute='*/10')
+        'schedule': crontab(minute='*/5')
     },
     # 'add_work_credential': {
     #     'task': 'fb_parser.tasks.add_work_credential',
@@ -30,12 +30,12 @@ app.conf.beat_schedule = {
     'add_proxy': {
         # 'task': 'fb_parser.tasks.add_proxy',
         'task': 'fb_parser.tasks.update_proxy',
-        'schedule': crontab(minute='*/10')
+        'schedule': crontab(minute='*/5')
     },
 
     'check_not_available_accounts': {
         'task': 'fb_parser.tasks.check_not_available_accounts',
         'schedule': crontab(
-            minute='*/10')
+            minute='*/5')
     },
 }
