7e2197247a750de943ff637fa1cfd63c9223c1c7
oleg <olegsh-gusev@mail.ru>
2021-07-01 13:27:41 +0300

    test

--- fb_parser/celery/celery.py
+++ fb_parser/celery/celery.py
@@ -19,10 +19,10 @@ app.conf.beat_schedule = {
         'task': 'fb_parser.tasks.start_first_update_posts',
         'schedule': crontab(minute='*/10')
     },
-    # 'start_parsing_by_keyword': {
-    #     'task': 'fb_parser.tasks.start_parsing_by_keyword',
-    #     'schedule': crontab(minute='*/5')
-    # },
+    'start_parsing_by_keyword': {
+        'task': 'fb_parser.tasks.start_parsing_by_keyword',
+        'schedule': crontab(minute='*/5')
+    },
     # 'add_work_credential': {
     #     'task': 'fb_parser.tasks.add_work_credential',
     #     'schedule': crontab(minute='*/1')
@@ -32,10 +32,10 @@ app.conf.beat_schedule = {
         'task': 'fb_parser.tasks.update_proxy',
         'schedule': crontab(minute='*/4')
     },
-    #
-    # 'check_not_available_accounts': {
-    #     'task': 'fb_parser.tasks.check_not_available_accounts',
-    #     'schedule': crontab(
-    #         minute='*/5')
-    # },
+
+    'check_not_available_accounts': {
+        'task': 'fb_parser.tasks.check_not_available_accounts',
+        'schedule': crontab(
+            minute='*/5')
+    },
 }
