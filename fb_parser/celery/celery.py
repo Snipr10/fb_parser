74245586609920da91d91414fe39c55f3060c96f
oleg <olegsh-gusev@mail.ru>
2021-07-01 15:20:11 +0300

    test

--- fb_parser/celery/celery.py
+++ fb_parser/celery/celery.py
@@ -19,10 +19,10 @@ app.conf.beat_schedule = {
         'task': 'fb_parser.tasks.start_first_update_posts',
         'schedule': crontab(minute='*/10')
     },
-    # 'start_parsing_by_keyword': {
-    #     'task': 'fb_parser.tasks.start_parsing_by_keyword',
-    #     'schedule': crontab(minute='*/5')
-    # },
+    'start_parsing_by_keyword': {
+        'task': 'fb_parser.tasks.start_parsing_by_keyword',
+        'schedule': crontab(minute='*/5')
+    },
     # 'add_work_credential': {
     #     'task': 'fb_parser.tasks.add_work_credential',
     #     'schedule': crontab(minute='*/1')
