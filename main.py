4a0d0a7b642269bfb165c70254eb675b97acb471
oleg <gusevoleg@gmail.com>
2022-05-14 17:50:54 +0300

    test

---
+++ main.py
@@ -0,0 +1,47 @@
+import json
+import multiprocessing
+import random
+import time
+
+from concurrent.futures.thread import ThreadPoolExecutor
+import threading
+import concurrent
+
+import telebot
+from django.utils import timezone
+
+import os
+
+from django.db.models import Q, F
+
+BOT = None
+
+Task = []
+
+if __name__ == '__main__':
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gms_tg.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    print(1)
+    import django
+
+    django.setup()
+
+    futures = []
+    #
+    # x = threading.Thread(target=update_, args=(0,))
+    # x.start()
+    from core.models import Account
+
+    a = Account.objects.get(id=321)
+    print(a)
+    print(a.cookie)
+    json.loads(a.cookie)
+    print(json.loads(a.cookie))
