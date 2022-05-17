1de0d0a79131b39c763daed1826caec2b22f793d
oleg <gusevoleg@gmail.com>
2022-05-17 09:59:27 +0300

    test

--- main.py
+++ main.py
@@ -20,6 +20,25 @@ BOT = None
 
 Task = []
 
+
+def new_process_key(i):
+    for i in range(5):
+        time.sleep(random.randint(3, 9))
+
+        print(f"multiprocessing {i}")
+        x = multiprocessing.Process(target=start_parsing_by_keyword, args=())
+        x.start()
+
+
+def new_process_source(i):
+    for i in range(5):
+        time.sleep(random.randint(3, 9))
+
+        print(f"multiprocessing {i}")
+        x = multiprocessing.Process(target=start_parsing_by_source, args=())
+        x.start()
+
+
 if __name__ == '__main__':
 
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fb_parser.settings')
@@ -43,15 +62,26 @@ if __name__ == '__main__':
     from core.models import Account
     from fb_parser.tasks import start_parsing_by_keyword, start_parsing_by_source
 
-    while True:
-        try:
-            print("start_parsing_by_source")
-            start_parsing_by_source()
-            print("start_parsing_by_keyword")
-            start_parsing_by_keyword()
-            django.db.close_old_connections()
-        except Exception as e:
-            print(e)
+    for i in range(2):
+        time.sleep(10)
+        print("thread new_process_key " + str(i))
+        x = threading.Thread(target=new_process_key, args=(i,))
+        x.start()
+    for i in range(2):
+        time.sleep(10)
+        print("thread new_process_source " + str(i))
+        x = threading.Thread(target=new_process_source, args=(i,))
+        x.start()
+
+    # while True:
+    #     try:
+    #         print("start_parsing_by_source")
+    #         start_parsing_by_source()
+    #         print("start_parsing_by_keyword")
+    #         start_parsing_by_keyword()
+    #         django.db.close_old_connections()
+    #     except Exception as e:
+    #         print(e)
         # time.sleep(5*60)
     # a = Account.objects.get(id=321)
     # print(a)
@@ -66,8 +96,3 @@ if __name__ == '__main__':
     #     print(z)
 
 
-    a = Account.objects.get(id=321)
-    print(a)
-    print(a.cookie)
-    json.loads(a.cookie)
-    print(json.loads(a.cookie))
