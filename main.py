0d001d30b64f1d8d3ee25daaf38e98c76c2d311a
oleg.gusev <gusevoleg@gmail.com>
2022-07-29 20:10:52 +0300

    added last_modified

--- main.py
+++ main.py
@@ -97,11 +97,11 @@ if __name__ == '__main__':
     x = threading.Thread(target=new_process_account_item, args=(0, ))
     x.start()
     #
-    for i in range(2):
-        time.sleep(10)
-        print("thread new_process_source " + str(i))
-        x = threading.Thread(target=new_process_source, args=(i, True,))
-        x.start()
+    # for i in range(2):
+    #     time.sleep(10)
+    #     print("thread new_process_source " + str(i))
+    #     x = threading.Thread(target=new_process_source, args=(i, True,))
+    #     x.start()
 
     for i in range(2):
         time.sleep(10)
