ae376cbb4ed19a1b347738b3fb20de2270880f46
oleg <oleg.gusev@gmail.com>
2023-02-03 18:36:41 +0300

    update

--- fb_parser/parser_data/data.py
+++ fb_parser/parser_data/data.py
@@ -255,10 +255,10 @@ def search_source(face_session, account, source, retro):
                     print(e)
         except Exception as e:
             print(f"search_source {source} {e}")
-            if "404 Client Error: Not Found for url: https://m.facebook.com/" in str(e):
-                # source.disabled = 1
+            if "404 Client Error: Not Found for url: https://m.facebook.com/" in str(e) or "Exceeded 30 redirects" in str(e) or "404 Client Error: Not Found for url:" in str(e):
+                source.disabled = 1
                 source.save(update_fields=["disabled"])
-            if "Content Not Found" in str(e):
+            elif "Content Not Found" in str(e):
                 source.taken = 0
                 source.last_modified = update_time_timezone(timezone.localtime())
                 source.save(update_fields=["last_modified", "taken"])
