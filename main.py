30b8e900e52aaa0995a98cec54fa21928c2e4b05
oleg <oleg.gusev@gmail.com>
2023-03-31 14:58:53 +0300

    start key parsing

--- main.py
+++ main.py
@@ -112,12 +112,12 @@ if __name__ == '__main__':
         x = threading.Thread(target=new_process_source, args=(i, False,))
         x.start()
 
-    # for i in range(2):
-    #     time.sleep(10)
-    #     print("thread new_process_key " + str(i))
-    #     x = threading.Thread(target=new_process_key, args=(i, False,))
-    #     x.start()
-    # #
+    for i in range(2):
+        time.sleep(10)
+        print("thread new_process_key " + str(i))
+        x = threading.Thread(target=new_process_key, args=(i, False,))
+        x.start()
+    #
     # for i in range(2):
     #     time.sleep(10)
     #     print("thread new_process_key " + str(i))
