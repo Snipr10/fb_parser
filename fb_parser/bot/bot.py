002de9fb018445b7025114bd99453cd0b783e34b
oleg.gusev <oleg.gusev@gmail.com>
2024-01-06 23:02:30 +0300

    database

--- fb_parser/bot/bot.py
+++ fb_parser/bot/bot.py
@@ -37,7 +37,7 @@ def get_session(is_special=False, is_join=False):
                                                 is_join_group=0,
                                                 special_group=0,
                                                 last_parsing__lte=update_time_timezone(
-                                                    timezone.now() - datetime.timedelta(minutes=20)),)
+                                                    timezone.now() - datetime.timedelta(minutes=20)),proxy_id__isnull=False)
     print("213214214")
     if is_join:
         account = models.Account.objects.filter(taken=0, banned=0,
